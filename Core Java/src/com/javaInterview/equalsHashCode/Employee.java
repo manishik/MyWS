08b82e6603f1c8a981f0c53f18cdc4ac4e290800
Manish Keshav <manishkeshav@Manishs-MacBook-Pro.local>
2019-01-03 17:29:33 -0500

    Changed age to unique ssn number

--- Core Java/src/com/javaInterview/equalsHashCode/Employee.java
+++ Core Java/src/com/javaInterview/equalsHashCode/Employee.java
@@ -2,20 +2,21 @@ package com.javaInterview.equalsHashCode;
 
 public class Employee {
 
-	private int age;
+	private String name;
+	private int ssn;
 
-	public Employee(int age) {
-		this.age = age;
+	public Employee(int ssn) {
+		this.ssn = ssn;
 	}
 
 	public boolean equals(Object obj) {
 		Employee emp = (Employee) obj;
-		if (emp.age == this.age)
+		if (emp.ssn == this.ssn)
 			return true;
 		return false;
 	}
 
 	public int hashCode() {
-		return age;
+		return ssn;
 	}
 }
