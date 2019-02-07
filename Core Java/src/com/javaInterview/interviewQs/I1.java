a0f6667b2d52a98e66678a3ef55281c37f148d56
manishkeshav <manishkeshav@manishs-mbp.hsd1.md.comcast.net>
2019-02-06 19:53:46 -0500

    Changed

--- Core Java/src/com/javaInterview/interviewQs/I1.java
+++ Core Java/src/com/javaInterview/interviewQs/I1.java
@@ -1,10 +1,31 @@
 package com.javaInterview.interviewQs;
 
-public class Comcast {
+public class I1 {
 
 	public static void main(String[] args) {
+
+		int num = 1011101001; // count max 1's between 2 zero's
+
+		String str = Integer.toString(num);
+
+		System.out.println("Number = " + num);
+
+		System.out.println("Max One Count = " + cal(str));
+
+	}
+
+	static int cal(String string) {
+
+		int oneCounter = 0; 
 		
-		int num = 1011101011; //count max 1's between 2 zero's 
+		System.out.println("length = " + string.length());
+		for (int i = 0; i < string.length(); i++) {
+			char c = string.charAt(i);
+			if (c == '0') {
+				oneCounter++;
+			}
+		}
+		return oneCounter;
 	}
 
 }
