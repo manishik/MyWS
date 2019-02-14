6c14efea8479dd4464ac60c7423a89a617f64f86
Manish <manish.indudhar@gmail.com>
2019-02-14 14:46:01 -0500

    renamed variable...

--- Core Java/src/com/java8/StreamPrg1.java
+++ Core Java/src/com/java8/StreamPrg1.java
@@ -7,9 +7,9 @@ import java.util.stream.Collectors;
 public class StreamPrg1 {
 
 	public static void main(String[] args) {
-		List<Integer> number = Arrays.asList(2, 3, 4, 5);
-		System.out.println("List of Number = " + number);
-		List<Integer> square = (List<Integer>) number.stream().map(x -> x * x).collect(Collectors.toList());
+		List<Integer> listOfInts = Arrays.asList(2, 3, 4, 5);
+		System.out.println("List of Number = " + listOfInts);
+		List<Integer> square = (List<Integer>) listOfInts.stream().map(x -> x * x).collect(Collectors.toList());
 		System.out.println("List of Squares = " + square);
 	}
 
