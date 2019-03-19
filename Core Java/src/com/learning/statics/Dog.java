a93fda5cbee3e0104f6b509ede640694f184f551
Manish <manish.indudhar@gmail.com>
2019-03-19 14:11:11 -0500

    Changes...

--- Core Java/src/com/learning/statics/Dog.java
+++ Core Java/src/com/learning/statics/Dog.java
@@ -2,25 +2,30 @@ package com.learning.statics;
 
 class Animal {
 	static void doStuff() {
-		System.out.print("a ");
+		System.out.print("animal. ");
 	}
 }
 
 public class Dog extends Animal {
-	static void dostuff() { // it's a redefinition,
-		// not an override
-		System.out.print("d ");
+
+	static void dostuff() { // it's a redefinition, not an override
+		System.out.print("dog. ");
 	}
 
 	public static void main(String[] args) {
-		/*Animal[] a = { new Animal(), new Dog(), new Animal() };
-		for (int x = 0; x < a.length; x++)
+
+		Animal[] a = { new Animal(), new Dog(), new Animal() };
+		for (int x = 0; x < a.length; x++) {
 			a[x].doStuff(); // invoke the static method
-*/
-		Animal a = new Dog();
-		
-		Dog dd = (Dog)a;
-		dd.dostuff();
-		((Dog)a).doStuff();
+		}
+
+		Animal animalObjRef = new Dog();
+
+		animalObjRef.doStuff();
+
+		((Dog) animalObjRef).doStuff();
+
+		Dog dogObj = (Dog) animalObjRef;
+		dogObj.dostuff();
 	}
 }
